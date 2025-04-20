6cfcc4c5d475cb787c6e285285b70be5ec101f6e
MRERROR2050 <abdalfatah.aljuaidi@gmail.com>
2025-04-20 19:23:14 +0300

    dssda

--- Frontend/src/components/Index/FromAdmin.jsx
+++ Frontend/src/components/Index/FromAdmin.jsx
@@ -3,21 +3,20 @@ import Nav from '../Index/Nav';
 import Menu from './Menu';
 import Extra from './Extra';
 import OfficialPosts from './OfficialPosts'; // تأكد أنك أنشأت هذا الملف
-import FromAdmin from './FromAdmin';
 
-const IndexTwo = () => {
+const FromAdmin = () => {
   const [activeSection, setActiveSection] = useState('posts'); // الحالة
 
   return (
     <div className='bg-slate-50'>
       <Nav setActive={setActiveSection} />
       <div className='flex justify-center'>
-        <Menu  setActiveSection={setActiveSection} isSticky={true}  />
-       <FromAdmin />
+        <Menu isSticky={true} />
+       <OfficialPosts />
         <Extra />
       </div>
     </div>
   );
 };
 
-export default IndexTwo;
+export default FromAdmin;
